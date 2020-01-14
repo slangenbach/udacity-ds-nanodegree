6e8672e6d2d59afa35b246f0f8b5dd0c27db1cf7
Stefan Langenbach <stefan.buenten@gmail.com>
2020-01-14 13:14:21 +0100

    Change topic names

--- traffic_optimization/consumers/models/line.py
+++ traffic_optimization/consumers/models/line.py
@@ -56,15 +56,15 @@ class Line:
 
     def process_message(self, message):
         """Given a kafka message, extract data"""
-        if message.topic == "^com.udacity.projects.transport.stations.*":
+        if message.topic == "^org.chicago.cta.stations.":
             try:
                 value = json.loads(message.value())
                 self._handle_station(value)
             except Exception as e:
                 logger.fatal("bad station? %s, %s", value, e)
-        elif message.topic == "^com.udacity.projects.transport.arrivals":
+        elif message.topic == "^org.chicago.cta.arrivals.":
             self._handle_arrival(message)
-        elif message.topic == "^com.udacity.projects.transport.turnstiles.*":
+        elif message.topic == "org.chicago.cta.turnstiles":
             json_data = json.loads(message.value())
             station_id = json_data.get("STATION_ID")
             station = self.stations.get(station_id)
