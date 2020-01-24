a6bc67264f20de8dc069e6e8d6ec0221a6d4ef21
Stefan Langenbach <stefan.buenten@gmail.com>
2020-01-24 15:37:43 +0100

    Set JAVA_HOME to OpenJDK 8 when executing Spark job

--- crime_statistics/submit_spark_job.sh
+++ crime_statistics/submit_spark_job.sh
@@ -1,2 +1,5 @@
 #!/bin/zsh
+echo "Setting JAVA_HOME"
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/
+echo "Submitting Spark job"
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.1 spark_streaming.py
